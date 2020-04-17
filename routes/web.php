7a03e0ee57b9eb575f1c5812aa65ed3aefb47ea6
AidaAsh <56032045+AidaAsh@users.noreply.github.com>
2020-04-17 21:59:33 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,47 +14,65 @@ use App\Course;
 use Illuminate\Http\Request;
 
 Route::auth();
+Auth::routes();
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/workers', 'WorkerController@index')->name('workers');
-
-Route::get('/workersForAccountant', 'WorkerController@indexForAccountant')->name('workers');
-
+// Route::get('/workers', 'WorkerController@index')->name('workers');
+// Route::get('/workersForAccountant', 'WorkerController@indexForAccountant')->name('workers');
 
 
-Route::post('/workers/add', 'WorkerController@store');
+// Route::post('/workers/add', 'WorkerController@store');
+// Route::post('/workers/addForAccountant', 'WorkerController@storeForAccountant');
 
 
-Route::get('/workers/add', 'WorkerController@create');
-
-Route::delete('/workers/delete/{worker}', 'WorkerController@destroy');
-
-
-Route::get('/workers/edit/{worker}', 'WorkerController@edit');
-
-Route::get('/workers/editForAccountant/{worker}', 'WorkerController@editForAccountant');
+// Route::get('/workers/add', 'WorkerController@create');
+// Route::get('/workers/addForAccountant', 'WorkerController@createForAccountant');
 
 
+// Route::get('/workers/edit/{worker}', 'WorkerController@edit');
+// Route::get('/workers/editForAccountant/{worker}', 'WorkerController@editForAccountant');
 
-Route::patch('/workers/update/{worker}', 'WorkerController@update');
-Auth::routes();
+
+// Route::patch('/workers/update/{worker}', 'WorkerController@update');
+// Route::patch('/workers/updateForAccoutant/{worker}', 'WorkerController@updateForAccoutant');
+
+// Route::any('/search', 'WorkerController@search')->name('workers');
+// Route::any('/searchForAccountant', 'WorkerController@searchForAccountant')->name('workers');
+
+// Route::delete('/workers/delete/{worker}', 'WorkerController@destroy');
+
+
+
 
 Route::get('/home', 'HomeController@index')->name('home');
 
 
-
 Route::group(['middleware' => 'App\Http\Middleware\AdminMiddleware'], function()
 {
-Route::match(['get', 'post'], '/adminOnlyPage/', 'WorkerController@admin');
+Route::match(['get', 'post'], '/adminOnlyPage/',  'WorkerController@admin');
+Route::any('/search', 'WorkerController@search')->name('workers');
+Route::match(['get', 'post'], '/workers', 'WorkerController@index')->name('workers');
+Route::match(['post'], '/workers/add', 'WorkerController@store');
+Route::match(['get'], '/workers/add', 'WorkerController@create');
+Route::match(['get', 'post'], '/workers/edit/{worker}', 'WorkerController@edit');
+Route::delete('/workers/delete/{worker}', 'WorkerController@destroy');
+Route::match(['get', 'post', 'patch'], '/workers/update/{worker}', 'WorkerController@update');
 });
-
-// Route::get('/courses', 'CourseController@index')->name('courses')->middleware(StudentMiddleware::class);
 
 
 Route::group(['middleware' => 'App\Http\Middleware\AccountantMiddleware'], function()
 {
 Route::match(['get', 'post'], '/accountantOnlyPage/', 'WorkerController@accountant');
+Route::any('/searchForAccountant', 'WorkerController@searchForAccountant')->name('workers');
+Route::match(['get', 'post'], '/workersForAccountant', 'WorkerController@indexForAccountant')->name('workers');
+Route::match(['post'], '/workers/addForAccountant', 'WorkerController@storeForAccountant');
+Route::match(['get'], '/workers/addForAccountant', 'WorkerController@createForAccountant');
+Route::delete('/workers/deleteForAccountant/{worker}', 'WorkerController@destroyForAccountant');
+Route::match(['get', 'post'], '/workers/editForAccountant/{worker}', 'WorkerController@editForAccountant');
+Route::match(['get', 'post', 'patch'], '/workers/updateForAccoutant/{worker}', 'WorkerController@updateForAccoutant');
 });
+
+
