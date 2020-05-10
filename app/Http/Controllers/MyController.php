03f50c0e19fe98a0a9084d82c953eaf928fc173a
AidaAsh <SoranoYumiko@gmail.com>
2020-05-11 03:45:51 +0600

    edrty

---
+++ app/Http/Controllers/MyController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Exports\VisitsExport;
+use App\Imports\VisitsImport;
+use Maatwebsite\Excel\Facades\Excel;
+
+class MyController extends Controller
+{
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function importExportView()
+    {
+       return view('import');
+    }
+
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function export()
+    {
+        return Excel::download(new UsersExport, 'visits.xlsx');
+    }
+
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function import()
+    {
+        Excel::import(new VisitsImport,request()->file('file'));
+
+        $visits = Visit::select('*')
+        ->get();
+
+        return view('workers.visit', ['visits' => $visits]);
+    }
+}
